dc0b3bed4d81deb50e86e3096ebbda7e71f49c5a
adolea <dolea15@gmail.com>
2020-04-19 05:58:48 +0100

    --cleanup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/option.hpp>
 
 #include <pistache/net.h>
 #include <pistache/http.h>
@@ -9,24 +7,12 @@
 
 #include "hello_world_api.h"
 
-namespace po = boost::program_options;
-
-class HelloHandler : public Pistache::Http::Handler {
-public:
-
-HTTP_PROTOTYPE(HelloHandler)
-
-    void onRequest(const Pistache::Http::Request& request, Pistache::Http::ResponseWriter response) {
-        response.send(Pistache::Http::Code::Ok, "Muie ba !!!");
-    }
-};
-
 int main() {
     Pistache::Address addr(Pistache::Ipv4::any(), Pistache::Port(9080));
 
-    auto opts =Pistache::Http::Endpoint::options().threads(1);
+    auto endpointOptions = Pistache::Http::Endpoint::options().threads(1);
     const auto server = std::make_shared<Pistache::Http::Endpoint>(addr);
-    server->init(opts);
+    server->init(endpointOptions);
 
     HelloWorldApi helloWorldApi("Aici, un mesaj dubios...");
     helloWorldApi.setupEndpointHandlers(server);
